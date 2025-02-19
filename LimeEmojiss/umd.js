9f72b5f079457cce61cb552d0c347c42c27a3082
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2025-02-19 20:11:21 +0800

    Update umd.js

--- LimeEmojiss/umd.js
+++ LimeEmojiss/umd.js
@@ -4,15 +4,13 @@ LimeEmojiss.repoApiUrl = 'https://api.github.com/repos/LimeWebb/emojis/contents/
 
 LimeEmojiss.getEmojiSet = async function(emojiSet='lime1111') {
   try {
-    const directories = await (await fetch(LimeEmojiss.repoApiUrl)).json()
-      .filter((file) => file.type === 'dir')
-      .map((directory) => directory.name)
+    const directories = await (await fetch(`${LimeEmojiss.repoApiUrl}${emojiSet}`)).json()
+      .filter((file) => file.type === 'file')
+    
+    if(!directories) return []
     
     if(Array.isArray(directories)) {
-      const dir = directories.find((d) => d.name == emojiSet)
-      if(!dir) return []
-      const emojis = await (await fetch(dir)).json()
-        .filter((emoji) => emoji.type === 'file')
+      const emojis = directories
         .map((emoji) => ({
           name: emoji.name.replace(/(\.png)$/, ''),
           url: emoji.download_url ?? null
@@ -31,6 +29,6 @@ LimeEmojiss.getEmojiByName = async function(name='lime1111_dizzy') {
   const split = name.split('_')
   const setName = split[0]
   const emojis = await LimeEmojiss.getEmojiSet(setName)
-  const emoji = emojis.find((e) => e.name == split[1]).url
+  const emoji = emojis.find((e) => e.name == split[1])?.url
   return emoji ?? ''
 }
